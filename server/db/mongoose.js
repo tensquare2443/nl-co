e394692e050d1f851fba807b2ba2ddd9713e6e1e
grklp3 <32232873+grklp3@users.noreply.github.com>
2018-04-03 14:07:56 -0400

    ...

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -5,10 +5,14 @@ mongoose.Promise = global.Promise;
 
 var databaseUri;
 if (process.env.PROD_MONGODB) {
+  console.log('ENV VAR TRUE ' + process.env.PROD_MONGODB);
   databaseUri = process.env.PROD_MONGODB;
 } else {
+  console.log('ENV VAR FALSE');
   databaseUri = 'mongodb://localhost:27017/NLCO';
 }
+
+console.log('PROD_MONGODB = ' + databaseUri);
 
 mongoose.connect(databaseUri, {
   useMongoClient: true
